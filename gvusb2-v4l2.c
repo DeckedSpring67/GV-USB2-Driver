669544cdb8b0b7602e077892f181c27ab77d29a6
Marco Martini <deckedspring@gmail.com>
2020-02-16 02:00:05 +0100

    Print in long long

--- gvusb2-v4l2.c
+++ gvusb2-v4l2.c
@@ -34,10 +34,7 @@ struct i2c_regval {
 
 void get_resolution(struct gvusb2_vid *dev, int *width, int *height)
 {
-	FILE *fp;
-	fp = fopen("/tmp/gvusb.log", "a");
-	fprintf(fp,"%s",dev->standard);
-	fclose(fp);
+	printk("%lld\n",dev->standard);
 	switch (dev->standard) {
 	default:
 	case V4L2_STD_NTSC_M:
